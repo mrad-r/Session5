c15fb2db5f18cdaa974b381455a845270d7c0157
matilderibeiro <matilde.csribeiro@gmail.com>
2025-01-24 12:48:31 +0100

    if example

--- if_example.py
+++ if_example.py
@@ -13,11 +13,11 @@ try:
     age = input("How old are you? ")
     age = int(age) # this is where we can have problems
     legal = None
-    country = input("where are you from?")
+    country = input("where are you from? ")
     if age < 14:
        legal = False
     elif age < 16:
-        if country == "Austria"
+        if country == "Austria":
             legal = True
         else:
             legal = False
@@ -29,8 +29,13 @@ try:
     elif age < 21:
         if country == "USA" or country == "UAE":
             legal = False
-        else:
-            legal = True
+    else:
+        legal = True
+
+    if legal:
+        print(f"Here is a {choice(drinks)} {choice(mixers)}")
+    else:
+        print(f"I can only serve you {choice(mixers)}")
 
 except ValueError:
     print("I don't have time for your games! Get out!")
